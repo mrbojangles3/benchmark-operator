535925f5cb8940879593b56442407c1c6be98cfa
Logan Blyth <lblyth@redhat.com>
2021-01-06 16:16:03 -0500

    removed test script

--- wrap_n_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-echo "you might need to do a quay login, check your notes"
-
-operator-sdk-v0.19.4-x86_64-linux-gnu build quay.io/lblyth/benchmark-operator --image-builder podman
-
-podman push quay.io/lblyth/benchmark-operator
