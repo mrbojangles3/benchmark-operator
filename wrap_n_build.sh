e5f70b7a011d51befe55d4d0485b82890ddf2c54
Logan Blyth <lblyth@redhat.com>
2021-02-15 13:20:42 -0500

    image create command

---
+++ wrap_n_build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -x
+echo "you might need to do a quay cli login, check your notes"
+
+operator-sdk-v0.19.4-x86_64-linux-gnu build quay.io/lblyth/benchmark-operator --image-builder podman
+
+podman push quay.io/lblyth/benchmark-operator
